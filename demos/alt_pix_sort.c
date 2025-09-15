3479cbe93703fb18941b973fe00294df62722fb8
Ben Y. <bjy777101@gmail.com>
2025-09-15 18:57:15 +1000

    Improved pix sort functions

--- demos/alt_pix_sort.c
+++ demos/alt_pix_sort.c
@@ -20,12 +20,17 @@ int cmp_t3(tup3 *a, tup3 *b)
     return (a_avg - b_avg >= 0.0);
 }
 
+#define WINDOW_SIZE (4)
+#define INTERLEAVE_GAP (4)
 
-#define MAX_WINDOW_SIZE (128)
-#define WINDOW_GROWTH (3)
-
+// Sort (1-n elements)
 void swap_sort(tup3 *arr, int size)
 {
+    if (size < 2 || size > WINDOW_SIZE)
+    {
+        return;
+    }
+
     // Naive O(n^2) min sort
     for (int i = 0; i < size; i++)
     {
@@ -33,7 +38,7 @@ void swap_sort(tup3 *arr, int size)
         int smallest_i = i;
         for (int j = i + 1; j < size; j++)
         {
-            if (cmp_t3(arr + smallest_i, arr + j))
+            if (! cmp_t3(arr + smallest_i, arr + j))
             {
                 smallest_i = j;
             }
@@ -47,23 +52,32 @@ void swap_sort(tup3 *arr, int size)
 }
 
 
-/* Widening window sort */
+/*
+ * Pixel sort that alternates between fixed local and interleaved windows
+ */
 tup3 fragment(tup3 *frag_coord)
 {
-    tup3 samples[MAX_WINDOW_SIZE];
+    tup3 samples[WINDOW_SIZE];
     int n_samples = 0;
-    int window_size = 3 * (FRAME_COUNT + 1);
-
-    window_size = window_size > MAX_WINDOW_SIZE ? MAX_WINDOW_SIZE : window_size;
 
     int self_index = (int) frag_coord->y;
-    int start_y = (self_index / window_size) * window_size;
 
-    // Load in pixel window
-    for (; n_samples < window_size && ! framebuf_read(BACKBUF, (int) frag_coord->x, start_y + n_samples, samples + n_samples); n_samples++);
+    // Even frame - local window
+    if (! (FRAME_COUNT % 2))
+    {
+        int start_y = (self_index / WINDOW_SIZE) * WINDOW_SIZE;
+        // Load in local pixel window
+        for (; n_samples < WINDOW_SIZE && ! framebuf_read(BACKBUF, (int) frag_coord->x, start_y + n_samples, samples + n_samples); n_samples++);
+    }
+    else
+    {
+        int start_y = (self_index / WINDOW_SIZE) * WINDOW_SIZE + (self_index % INTERLEAVE_GAP);
+        // Load in interleaved pixel window
+        for (; n_samples < WINDOW_SIZE && ! framebuf_read(BACKBUF, (int) frag_coord->x, start_y + n_samples * INTERLEAVE_GAP, samples + n_samples); n_samples++);
+    }
 
     // Sort pixel window
     swap_sort(samples, n_samples);
 
-    return samples[(self_index % window_size)];
+    return samples[(self_index % WINDOW_SIZE)];
 }
